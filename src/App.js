b6dfd16364394d64e82f1b89009680e25dd860f6
HenrikDesktop <heha98@live.se>
2023-02-05 18:00:45 +0100

    Done-ish

--- src/App.js
+++ src/App.js
@@ -40,12 +40,16 @@ function App() {
           <p>Welcome to the weather app! Enter a city you want info on.</p>
         </div>
       ) : (
-        <div>
-          <p>{weatherData.name}</p>
-          <p>{Math.round(weatherData.main.temp)+"°C"}</p>
-          <p>{weatherData.weather[0].main}</p>
+        <div className="weather-data">
+          <p className="city">{weatherData.name}</p>
+          <p className="temp">{Math.round(weatherData.main.temp)+"°C"}</p>
+          <p className="weather">{weatherData.weather[0].main}</p>
         </div>
       )}
+
+      {weatherData.cod === "404" ? (
+        <p>City not found.</p>
+      ): (<></>)}
     </div>
   );
 }
