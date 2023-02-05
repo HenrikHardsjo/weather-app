8fdb0f0f1ebbb7d344a3f2568dddceb4695b7857
HenrikDesktop <heha98@live.se>
2023-02-05 17:51:42 +0100

    Fungerar rätt ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 //test
 
 function App() {
+  const apiKey = "7a4ad7b465225038fc5b97080cd918bf";
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (inputCity) =>
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        setCity("");
+        console.log(data);
+      });
+
   return (
-    <div className="App"> 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="inputBox"
+        placeholder="Enter a city..."
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+      />
+      <button
+        className="test-button"
+        onClick={() => {
+          getWeather(city);
+        }}
+      >
+        Sök
+      </button>
+    
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>Welcome to the weather app! Enter a city you want info on.</p>
+        </div>
+      ) : (
+        <div>
+          <p>{weatherData.name}</p>
+          <p>{Math.round(weatherData.main.temp)+"°C"}</p>
+          <p>{weatherData.weather[0].main}</p>
+        </div>
+      )}
     </div>
   );
 }
