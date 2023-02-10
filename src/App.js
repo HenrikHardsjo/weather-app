9423705e49b683a8fb01d0c96f6d1305753f4590
HenrikHardsjo <heha98@live.se>
2023-02-10 10:45:33 +0100

    Tagit bort

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App1.css";
-// import Main from "./Main.jsx";
-// import TestView from "./Views/TestView";
 import Searchbar from "./Components/Searchbar";
 import WeatherProvider from "./Contexts/WeatherContext";
 import WeatherView from "./Views/WeatherView";
@@ -14,7 +12,6 @@ function App() {
       <Searchbar/>
       <WeatherView/>
     </WeatherProvider>
-    // <Main/>
   );
 }
 
