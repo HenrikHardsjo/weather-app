8c82f191432876f34202c7bea30e3db643633767
HenrikHardsjo <heha98@live.se>
2023-02-10 14:32:49 +0100

    As good as it gets.

--- src/Components/ForecastCard1.jsx
+++ src/Components/ForecastCard1.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext} from "react";
 import { WeatherContext } from "../Contexts/WeatherContext";
 import TrimString from "./TrimString";
 
