0ac9588db28826f93032626c72aa3391864aeae7
HenrikHardsjo <heha98@live.se>
2023-09-18 21:13:17 +0200

    Will work?

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.get('/getWeather/:city', async (req, res) => {
     const city = req.params.city;
     try {
-        const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=a68cd35bad144edda19132135231304&q=stockholm&days=6&aqi=no&alerts=no`);
+        const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=a68cd35bad144edda19132135231304&q=${city}&days=6&aqi=no&alerts=no`);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch weather data' });
